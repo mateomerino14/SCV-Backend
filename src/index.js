7eb64b6fdf3c22faf810b197142c22bbbb307a83
MAT988 <mateomerino988@gmail.com>
2026-06-08 13:05:35 -0400

    fix:Ajuste de direccion de cors

--- src/index.js
+++ src/index.js
@@ -4,9 +4,13 @@ const cookieParser = require('cookie-parser')
 const app = express()
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
-  credentials: true,
-}))
+  origin: [
+    "http://localhost:5173",
+    "https://scv-frontend.vercel.app"
+  ],
+  credentials: true
+}));
+
 app.use(express.json())
 app.use(cookieParser())
 
